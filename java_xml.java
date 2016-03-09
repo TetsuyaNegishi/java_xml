ef1c998bbc2e67355c2c6540e217123fcbad7be7
Tetsuya Negishi <tetsuyanegishi@Tetsuya-Negishi.local>
2016-03-09 22:55:16 +0900

    get Node name

--- java_xml.java
+++ java_xml.java
@@ -1,11 +1,26 @@
+import java.io.*;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.*;
 
 public class java_xml {
     public static void main(String args[]){
-	System.out.println("tes");
-Document document= DocumentBuilderFactory
-                          .newInstance()
-                          .newDocumentBuilder()
-    .parse(new File("helloWorld.xml"));
+	try {
+	    Document document= DocumentBuilderFactory
+		.newInstance()
+		.newDocumentBuilder()
+		.parse(new File("test.xml"));
+	    Node node=document.getDocumentElement();
+	    NodeList nodes = node.getChildNodes();
+	    for(int i = 0; i < nodes.getLength(); i++){
+		String name = nodes.item(i).getNodeName();
+		System.out.println(name);
+	    }
+	} catch (Exception e) {
+	   
+	}
     }
 }
